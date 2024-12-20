3a0dbb175f70c8a83bca04f8f3969cf3ca649b2e
stalexsm <stalex.info@yandex.ru>
2024-12-20 09:41:46 +0300

    fix: Правим ключи в Style для JSON и в самой схеме

--- core_rs/src/structs/style.rs
+++ core_rs/src/structs/style.rs
@@ -2,18 +2,18 @@ use serde::Serialize;
 
 #[derive(Default, Debug, Clone, Serialize, PartialEq, Eq)]
 pub struct Style {
-    #[serde(rename = "style")]
-    style_id: String,
+    #[serde(rename = "style_id")]
+    id: String,
 }
 
 impl Style {
     pub fn new(val: &str) -> Self {
         Style {
-            style_id: val.to_string(),
+            id: val.to_string(),
         }
     }
 
     pub fn get_id(&self) -> String {
-        self.style_id.to_string()
+        self.id.to_string()
     }
 }
